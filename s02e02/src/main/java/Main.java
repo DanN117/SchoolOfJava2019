8c292de4111d8b6f045ea1e4947f163322833b5b
My Name <abc@123.com>
2019-04-11 20:07:54 +0300

    s02e02v06

--- s02e02/src/main/java/Main.java
+++ s02e02/src/main/java/Main.java
@@ -1,7 +1,25 @@
+import model.BankAccount;
+import repositories.BankAccountRepository;
+import services.BankAccountService;
+
 public class Main {
 
     public static void main (String[] args) {
 
+        // creeam 2 bank account, 2 obiecte java clasice
+        BankAccount account1 = new BankAccount();
+        BankAccount account2 = new BankAccount();   // cu date si chestii
+
+        // de introdus in "baza de date"
+
+        // clasa service
+        // fara controller
+        BankAccountService bankAccountService = context.getBean(BankAccountService.class);
+        bankAccountService.transferMoney("1", "2", 1337.f);  // de facut cum trebuie
+        // se fac validari in service
+
+
+        // system print out ambele conturi dupa transfer
 
     }
 }
