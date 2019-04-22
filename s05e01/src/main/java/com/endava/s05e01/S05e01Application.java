0bb757074113c1ebb9b6e4b268c95875d0167ba9
My Name <abc@123.com>
2019-04-22 20:03:05 +0300

    s05e01v02

--- s05e01/src/main/java/com/endava/s05e01/S05e01Application.java
+++ s05e01/src/main/java/com/endava/s05e01/S05e01Application.java
@@ -2,6 +2,8 @@ package com.endava.s05e01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class S05e01Application {
@@ -10,4 +12,8 @@ public class S05e01Application {
 		SpringApplication.run(S05e01Application.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate() {
+		RestTemplate
+	}
 }
--- s05e01/src/main/java/com/endava/s05e01/controller/MovieController.java
+++ s05e01/src/main/java/com/endava/s05e01/controller/MovieController.java
@@ -2,14 +2,19 @@ package com.endava.s05e01.controller;
 
 import com.endava.s05e01.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class MovieController {
-
-
     @Autowired
     private MovieService movieService;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+
     //CREATE
     //RequestBody
     //RequesttParam
@@ -22,7 +27,7 @@ public class MovieController {
 
     @PostMapping(value = "/add")
     public String addMovie (@RequestBody Movie movie) {
-        
+        httpServiceResponse(HttpStatus)
     }
 
 
@@ -37,4 +42,11 @@ public class MovieController {
 
         return 0;
     }
+
+    @GetMapping
+    public int callRest() {
+        String url = "http//localhost:8080//";
+        restTemplate.delete(url);
+    }
+
 }
